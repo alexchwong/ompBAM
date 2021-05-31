522bf2989b36d1c334386ecf8dc1cb8b24504ac7
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-05-31 20:25:57 +1000

    Bugfixes

--- inst/examples/ParaBAMExample/src/main.cpp
+++ inst/examples/ParaBAMExample/src/main.cpp
@@ -26,25 +26,41 @@ int test_ParaBAM(std::string bam_file, int n_threads_to_use = 1){
   
   inbam.fillReads();
   
+  uint32_t total_reads = 0;
+  
   #ifdef _OPENMP
   #pragma omp parallel for
   #endif
-  for(unsigned int i = 0; i < n_threads_to_use; i++) {
+  for(unsigned int i = 0; i < (unsigned int)n_threads_to_use; i++) {
     unsigned int read_counter = 0;
-    char * read = inbam.supplyRead(i);
-    while(read && read_counter < 10) {
-      uint8_t l_read_name;
-      char * read_name = inbam.readName(read, l_read_name);
-      std::string s_read_name(read_name, l_read_name);
-      
-      #pragma omp critical
-      Rcout << "thread " << i << " read " << s_read_name << '\n';
-      
-      read = inbam.supplyRead(i); // Next read
-      read_counter++;
-    }
-  }
+    char * read;
+    do {
+      read = inbam.supplyRead(i);
+      if(read) {
+        read_counter++;
+
+        // Prints the read names of first 10 reads of each thread
+        if(read_counter <= 10) {
+          uint8_t l_read_name;
+          char * read_name = inbam.readName(read, l_read_name);
+          std::string s_read_name(read_name, l_read_name);
+          
+          #ifdef _OPENMP
+          #pragma omp critical
+          #endif
+          Rcout << "thread " << i << " read " << s_read_name << '\n';
+        }
+
+      }
+    } while(read);
   
+    #ifdef _OPENMP
+    #pragma omp critical
+    #endif
+    total_reads += read_counter;
+  }
   inbam_stream.close();
+
+  Rcout << "Total reads = " << total_reads << '\n';
   return(0);
 }
