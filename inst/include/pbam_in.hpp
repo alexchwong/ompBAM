2ae07880b6d8a939011600ed1820e8a301e35458
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-09-07 10:06:48 +1000

    Add more debug msgs, this time to supplyRead
    Former-commit-id: eb760f1e2768b0e6ae883657ac05e72bd1bc40cd

--- inst/include/pbam_in.hpp
+++ inst/include/pbam_in.hpp
@@ -891,7 +891,16 @@ inline pbam1_t pbam_in::supplyRead(const unsigned int thread_number) {
     return(read);
   }
   read = pbam1_t(data_buf + read_cursors.at(thread_number), false);
-  if(read.validate()) read_cursors.at(thread_number) += read.block_size() + 4;
+  if(read.validate()) {
+    read_cursors.at(thread_number) += read.block_size() + 4;
+  } else {
+    // Check this is actually end of thread buffer; throw error here otherwise
+    if(read_cursors.at(thread_number) < read_ptr_ends.at(thread_number)) {
+      Rcpp::Rcout << "Invalid read found before end of thread buffer " 
+        << thread_number << ". read_cursor = " << read_cursors.at(thread_number)
+        << ", read_ptr_ends = " << read_ptr_ends.at(thread_number) << '\n';
+    }
+  }    
   return(read);
 }
 
