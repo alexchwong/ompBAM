7922f6babf5414687596c44434cea092cd959a67
alexchwong <80015046+alexchwong@users.noreply.github.com>
2022-03-12 14:25:53 +1100

    Update ompBAM.hpp
    * Remove dependency on Rcpp
    * Name change for ompBAM_version()

--- inst/include/ompBAM.hpp
+++ inst/include/ompBAM.hpp
@@ -1,3 +1,25 @@
+/* ompBAM.hpp ompBAM Library
+
+Copyright (C) 2021 Alex Chit Hei Wong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.  */
+
 #ifndef _ompBAM
 #define _ompBAM
 
@@ -10,28 +32,28 @@
 #include <string>    
 #include <cstring>    // To compare between strings
 #include <vector>     // For vector types
+#include <iostream>   // For cout
 
 #ifdef _OPENMP
   #include <omp.h>    // For OpenMP
 #endif
 
-#include "Rcpp.h"     // For Rcpp::Rcout
-
 #include "pbam_defs.hpp"
 #include "pbam1_t.hpp"
 #include "pbam_in.hpp"
 
-inline void paraBAM_version() {
+inline void ompBAM_version() {
   std::string version = "0.99.0";
-  Rcpp::Rcout << "Compiled using ompBAM version " << version << "\n";
+  cout << "Compiled using ompBAM version " << version 
+    << " for NxtIRFcore\n";
 }
 
 inline void is_compiled_with_OpenMP() {
   #ifdef _OPENMP
-    Rcpp::Rcout << "Compiled with OpenMP; " << omp_get_max_threads()
+    cout << "Compiled with OpenMP; " << omp_get_max_threads()
       << " threads available\n";
   #else
-    Rcpp::Rcout << "Compiled without OpenMP support\n";
+    cout << "Compiled without OpenMP support\n";
   #endif
 }
 
